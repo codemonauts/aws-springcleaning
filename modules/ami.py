1b4ee3d74257f90a10135d510c56502d1ac03367
Felix Breidenstein <felix@codemonauts.com>
2020-07-09 15:12:17 +0200

    ec2: ALso check launch templates for usage

--- modules/ami.py
+++ modules/ami.py
@@ -10,23 +10,37 @@ def scan():
 
     for region in REGIONS:
         # Filter only for our own images
-        ami_list = boto3.client("ec2", region_name=region).describe_images(Owners=[account_id])["Images"]
-        print("Found {} AMIs in {}".format(len(ami_list), region))
+        ec2_client = boto3.client("ec2", region_name=region)
+        ami_list = ec2_client.describe_images(Owners=[account_id])["Images"]
+        if len(ami_list):
+            print("Found {} AMIs in {}".format(len(ami_list), region))
+        else:
+            print("Found no AMIs in {}".format(len(ami_list), region))
+            continue
 
         used_images = []
-        if len(ami_list):
-            instances = get_all_instances([region])
-            for i in instances:
-                used_images.append(i["ImageId"])
+
+        # Get all running instances
+        instances = get_all_instances([region])
+        for i in instances:
+            used_images.append(i["ImageId"])
+
+        # Get all launch templates
+        resp = ec2_client.describe_launch_templates()
+        templates = resp.get("LaunchTemplates")
+        for t in templates:
+            resp = ec2_client.describe_launch_template_versions(LaunchTemplateName=t["LaunchTemplateName"])
+            used_images += [v["LaunchTemplateData"]["ImageId"] for v in resp["LaunchTemplateVersions"]]
 
         unused_images = []
         for ami in ami_list:
             if ami["ImageId"] not in used_images:
                 unused_images.append(ami)
-        if len(unused_images):
+
+        if len(unused_images) > 0:
             print("{} of them are not in use:".format(crayons.red(len(unused_images))))
             for ami in unused_images:
-                print("  - {:<30} ({})".format(ami["ImageId"], ami["Name"]))
+                print("  - {:<22} ({})".format(ami["ImageId"], ami["Name"]))
 
 
 if __name__ == "__main__":
