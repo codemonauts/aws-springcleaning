c7cdb015aa4bc709b293733bbf8bd9303416cb9a
Felix Breidenstein <felix@codemonauts.com>
2021-07-26 17:35:05 +0200

    lambda: Better colorcoding of runtime versions

--- modules/lambdafunctions.py
+++ modules/lambdafunctions.py
@@ -4,20 +4,23 @@ import arrow
 import crayons
 from config import REGIONS
 
-OUTDATED = [
-    "nodejs",
-    "nodejs4.3",
-    "nodejs6.10",
-    "nodejs8.10",
-    "nodejs10.x",
-    "java8",
-    "python2.7",
-    "python3.6",
-    "python3.7",
-    "dotnetcore1.0",
-    "dotnetcore2.0",
+LATEST = [
+    "dotnetcore3.1",
+    "java11",
+    "go1.x",
+    "nodejs14.x",
+    "python3.8",
+    "ruby2.7",
+]
+
+OTHER = [
     "dotnetcore2.1",
-    "nodejs4.3-edge",
+    "java8",
+    "java8.al2",
+    "nodejs10.x",
+    "nodejs12.x",
+    "python3.7",
+    "python3.6",
     "ruby2.5",
 ]
 
@@ -36,10 +39,12 @@ def scan():
         print("Found {} lambda functions in {}".format(len(functions), region))
         for f in functions:
             last_modified = arrow.get(f["LastModified"]).humanize()
-            if f["Runtime"] in OUTDATED:
-                runtime = crayons.red(f["Runtime"])
-            else:
+            if f["Runtime"] in LATEST:
                 runtime = f["Runtime"]
+            elif f["Runtime"] in OTHER:
+                runtime = crayons.yellow(f["Runtime"])
+            else:
+                runtime = crayons.red(f["Runtime"])
             print("  - {:<50} (Last modified: {}, {})".format(f["FunctionName"], last_modified, runtime))
 
 
